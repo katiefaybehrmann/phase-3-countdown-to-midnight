c66aa8acbcefbe87eca08cd7877246f8fe4af256
Katie <katiefaybehrmann@gmail.com>
2023-02-18 21:19:40 -0700

    commit tree e28a514c95c1ba58a40acb821e5c3e1b9931a993

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown (num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num = 10)
+    i = num
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i-= 1
+        sleep 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
